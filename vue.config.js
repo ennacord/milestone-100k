00bd3faae8b0429f154799c4e7a92c8f1f531874
dragonjet <jetriconew@gmail.com>
2021-10-31 23:32:49 -0700

    Vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,28 @@
-module.exports = {
-  lintOnSave: false,
+const path = require('path');
 
+module.exports = {
+  outputDir: path.resolve(__dirname, 'docs'),
+  lintOnSave: false,
   transpileDependencies: [
     'vuetify',
   ],
+  chainWebpack: (config) => {
+    // HTML Title used for OpenGraph linters (social media links)
+    config.plugin('html').tap((args) => {
+      // eslint-disable-next-line no-param-reassign
+      args[0].title = 'Enna Alouette 100k Subsribers Tribute';
+      // eslint-disable-next-line no-param-reassign
+      args[0].meta = {
+        'og:title': 'Enna Alouette 100k Subsribers Tribute',
+        'og:type': 'website',
+        'og:description': 'The community celebrates Enna Alouette reaching 100,000 YouTube subscribers.',
+        // 'og:image': '',
+        'twitter:card': 'summary_large_image',
+        'twitter:title': 'Enna Alouette 100k Subsribers Tribute',
+        'twitter:description': 'The community celebrates Enna Alouette reaching 100,000 YouTube subscribers.',
+        // 'twitter:image': '',
+      };
+      return args;
+    });
+  },
 };
